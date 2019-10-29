1a53633eee8d3312b563168d6fcfce5dc962d895
rajaborntohack <rajaborntohack@gmail.com>
2019-10-29 18:51:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/addition/Activity2.java
@@ -0,0 +1,25 @@
+package com.example.addition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    private TextView result1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        result1=(TextView)findViewById(R.id.tv1);
+        Intent intent = getIntent();
+
+        int answer = intent.getIntExtra("a", 0);
+
+
+        result1.setText("SUM=" + answer);
+    }
+}
